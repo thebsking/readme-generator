ac80e7f40e6b55d87622fcae7d551a5a27b5f276
Brandon King <brandonscottking@gmail.com>
2021-03-22 21:28:10 -0400

    adding base code

---
+++ index.js
@@ -0,0 +1,34 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'fileName',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'fileName',
+        message: ''
+    },
+    {
+        type: 'input',
+        name: 'fileName',
+        message: ''
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: ['']
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
