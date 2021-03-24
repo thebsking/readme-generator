8eff8a1d712c0f4b275fba0b56f697f936fbfd9b
Brandon King <brandonscottking@gmail.com>
2021-03-24 08:35:06 -0400

    added require statements; began questions array

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const generateMarkdown = require('generateMarkdown')
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'fileName',
-        message: ''
+        message: 'what is the name of your project?'
     },
     {
         type: 'input',
-        name: 'fileName',
-        message: ''
+        name: 'description',
+        message: 'please give a description of your project:'
+    },
+    {
+        type: 'checkbox',
+        name: 'tech',
+        message: 'what technologies did you use on this project? (Select all that apply)',
+        choices: ['HTML', 'CSS', 'JS', 'jQuery', 'node.js', 'npm', ]
     },
     {
         type: 'input',
-        name: 'fileName',
-        message: ''
+        name: 'link1',
+        message: 'please provide repository link'
+    },
+    {
+        type: 'input',
+        name: 'link2',
+        message: 'please provide link to deployed app'
     },
     {
         type: 'list',
@@ -25,10 +38,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    //function text
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    //function text
+}
 
 // Function call to initialize app
 init();
